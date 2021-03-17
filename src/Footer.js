b682bff6da3db2a12f8a837be738d9470d029f31
Ravennisis <76167228+Ravennisis@users.noreply.github.com>
2021-03-17 16:08:55 +0100

    changes to code to fit VS structure

--- src/Footer.js
+++ src/Footer.js
@@ -5,18 +5,18 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="code">
-      <a href="" target="_blank">
+      <a href="https://github.com/Ravennisis/weather-app-react" target="_blank" rel="noreferrer">
         Open-source code
       </a>{" "}
       by{" "}
       <a
         href="https://www.linkedin.com/in/ravenn-annibal-91811b201/"
-        target="_blank"
+        target="_blank" rel="noreferrer"
       >
         Ravenn Annibal
       </a>
       , hosted on{" "}
-      <a href="" target="_blank">
+      <a href="https://brave-wiles-2e5325.netlify.app" target="_blank" rel="noreferrer">
         Netlify
       </a>
     </div>
